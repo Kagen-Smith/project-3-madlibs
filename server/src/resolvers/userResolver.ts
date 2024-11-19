9e2b4187222058a70248466a454fb116b1107b78
Kagen Smith <kagensmith@Smiths-iMac.local>
2024-11-18 21:03:39 -0700

    finsihed resolvers

--- server/src/resolvers/userResolver.ts
+++ server/src/resolvers/userResolver.ts
@@ -1,28 +1,39 @@
 // @ts-ignore
-import { Arg, Ctx, Mutation, Query, Resolver } from 'type-graphql';
-import User from '../models/user';
-import UserInfo from './typedefs';
+import StoryInput from './typedefs';
+import User, { UserDocument } from '../models/user';
 import { StoryTemp } from '../models/storyTemplate';
 import { signToken, AuthenticationError } from '../config/jwt';
-import { login } from '../controllers/userController';
 
 const resolvers = {
     Query: {
-        me: async (_: any, __: any, context: any) => {
+        // Query to get a single user by ID or username
+        me: async (_parent: any, { _id, username }: {_id?: string, username?: string}, context: any): Promise<UserDocument | null> => {
+            // Check if user is authenticated 
             if (context.user) {
-                const userData = await User.findOne({ _id: context.user._id })
-                    .select('-__v -password')
-                    .populate('savedStories');
 
-                return userData;
+                // Find user by ID or username
+                const foundUser = await User.findOne({ 
+                    $or: [
+                        { _id: context.user._id}, 
+                        { _id }, 
+                        { username }
+                    ],
+                });
+
+                // If user is not found, throw an error
+                if (!foundUser) {
+                    throw new AuthenticationError('Cannot find a user with this id!');
+                }
+                return foundUser; // Return user if found
             }
 
-            throw new AuthenticationError('Not logged in');
+            // If user is not authenticated, throw an error
+            throw new AuthenticationError('You need to be logged in!');
         },
-    },
+},
 
     Mutation: {
-        login: async (_parent: any, { email, password }: any): Promise<{ token: string; user: typeof UserInfo } | null>  => {
+        login: async (_parent: any, { email, password }: any): Promise<{ token: string; user: UserDocument } | null>  => {
             // Find user by username or email
             const user = await User.findOne({ email });
 
@@ -41,7 +52,7 @@ const resolvers = {
             const token = signToken(user.username, user.email, user._id);
             return { token, user };
         },
-        addUser: async (_parent: any, args: any): Promise<{ token: string; user: typeof UserInfo } | null> => {
+        addUser: async (_parent: any, args: any): Promise<{ token: string; user: UserDocument } | null> => {
             // Create a new user
             const user = await User.create(args);
 
@@ -55,6 +66,32 @@ const resolvers = {
             // Return the token and user
             return { token, user };
         },
-        saveStory: async (_parent: any, { storyData }: { storyData: StoryTemp}, context: any) => {} 
+        saveStory: async (_parent: any, { storyData }: { storyData: StoryTemp}, context: any) => {
+            if (context.user) {
+                const updatedUser = await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $addToSet: { savedStories: storyData } },
+                    { new: true, runValidators: true }
+                );
+                return updatedUser;
+            } 
+            throw new AuthenticationError('You need to be logged in!');
+        },
+        removeStory: async (_parent: any, { storyId }: { storyId: string }, context: any) => {
+            if (context.user) {
+                const updatedUser = await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $pull: { savedStories: { storyId } } },
+                    { new: true }
+                );
+                return updatedUser;
+            }
+            throw new AuthenticationError('You need to be logged in!');
+        },
+    
+    },
+};
+
+export default resolvers;
 
 
