b8c38a3f43998e09f94f1a47531f285ae1997c38
parkerspeares <parker.speares@outlook.com>
2024-11-18 19:30:02 -0800

    profilepage.ts

--- client/src/pages/ProfilePage.ts
+++ client/src/pages/ProfilePage.ts
@@ -1,3 +1,6 @@
 import React, { useContext } from 'react';
 import Profile from '../components/Profile';
 import { AuthContext } from '../AuthContext';
+
+const ProfilePage: React.FC = () => {
+  const { user } = useContext(AuthContext); // Get user data from context
