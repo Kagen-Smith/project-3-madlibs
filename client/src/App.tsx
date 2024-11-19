65aa1a539a6018ca9e0419874fab77e1a2556112
Jacob Menlove <jacobmenlove1@gmail.com>
2024-11-19 00:26:03 -0500

    push for the night

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import viteLogo from './vite.svg'
 import './App.css'
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 import React, { useState } from 'react';
