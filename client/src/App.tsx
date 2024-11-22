5609bc366420d93bef80c4a4c083f51724e88f04
Jacob Menlove <jacobmenlove1@gmail.com>
2024-11-22 00:26:21 -0500

    fixing front end 2

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css';
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import TemplatesList from './components/templateCard';
 import PlayMadLib from './components/playMadlib';
 import CompletedStories from './components/madLibStory';
@@ -17,20 +18,71 @@ const App: React.FC = () => {
 
   return (
     <ApolloProvider client={client}>
-      <div>
-        <header>
-          <h1>Mad Libs</h1>
-        </header>
-        <main>
-          {!selectedTemplate && <TemplatesList onSelect={setSelectedTemplate} />}
-          {selectedTemplate && <PlayMadLib template={selectedTemplate} />}
-          <PhotoSearch /> {/* New Unsplash API Integration */}
-          <CompletedStories />
-        </main>
-        <footer>
-          <p>Powered by Vite + React + Unsplash</p>
-        </footer>
-      </div>
+      <Router>
+        <div>
+          {/* Navigation Bar */}
+          <header>
+            <h1>Mad Libs</h1>
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/">Templates</Link>
+                </li>
+                <li>
+                  <Link to="/play">Play Mad Lib</Link>
+                </li>
+                <li>
+                  <Link to="/photos">Photo Search</Link>
+                </li>
+                <li>
+                  <Link to="/stories">Completed Stories</Link>
+                </li>
+              </ul>
+            </nav>
+          </header>
+
+          {/* Main Content */}
+          <main>
+            <Routes>
+              {/* Templates List */}
+              <Route
+                path="/"
+                element={
+                  <TemplatesList
+                    onSelect={(template) => {
+                      setSelectedTemplate(template);
+                      window.location.href = '/play'; // Navigate to Play page when template is selected
+                    }}
+                  />
+                }
+              />
+
+              {/* Play Mad Lib */}
+              <Route
+                path="/play"
+                element={
+                  selectedTemplate ? (
+                    <PlayMadLib template={selectedTemplate} />
+                  ) : (
+                    <p>Please select a template first!</p>
+                  )
+                }
+              />
+
+              {/* Photo Search */}
+              <Route path="/photos" element={<PhotoSearch />} />
+
+              {/* Completed Stories */}
+              <Route path="/stories" element={<CompletedStories />} />
+            </Routes>
+          </main>
+
+          {/* Footer */}
+          <footer>
+            <p>Powered by Vite + React + Unsplash</p>
+          </footer>
+        </div>
+      </Router>
     </ApolloProvider>
   );
 };
